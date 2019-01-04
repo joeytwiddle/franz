f03f6ebacd63789e7d8b8e12d0e80f8f5fac477a
Paul Joey Clark <joeytwiddle@gmail.com>
2019-01-04 14:35:00 +0800

    Enable premium features?

--- src/models/User.js
+++ src/models/User.js
@@ -43,7 +43,7 @@ export default class User {
     this.lastname = data.lastname || this.lastname;
     this.organization = data.organization || this.organization;
     this.accountType = data.accountType || this.accountType;
-    this.isPremium = data.isPremium || this.isPremium;
+    this.isPremium = true; // data.isPremium || this.isPremium;
     this.beta = data.beta || this.beta;
     this.donor = data.donor || this.donor;
     this.isDonor = data.isDonor || this.isDonor;
